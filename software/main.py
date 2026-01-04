71570dae06358ca8218198496c8af98828f46a7b
Jacob Cunningham <jcun4128@gmail.com>
2026-01-03 20:48:33 -0600

    buttons interfaced

--- software/main.py
+++ software/main.py
@@ -2,12 +2,16 @@
 
 import time
 from imu import IMU
+from buttons import Buttons
 
 imu = IMU()
 imu.start()
 
+buttons = Buttons()
+buttons.start()
+
 while True:
-  print (imu.accel)
+  # print (imu.accel)
   time.sleep(0.1)
 
 
